73eef85b89f1333b4a67391c9a86ad71fc550fd4
Adam Drew <addrew@redhat.com>
2022-07-14 09:21:58 -0400

    Deploy debugger pod in ephemeral

--- scripts/buildAndDeployDebugger.sh
+++ scripts/buildAndDeployDebugger.sh
@@ -7,6 +7,6 @@ podman push quay.io/$QUAY_USERNAME/$QUAY_REPO:$TAG
 
 cat yaml/pod.yaml | sed 's/QUAY_REPO/'"$QUAY_REPO"'/' | sed 's/QUAY_USERNAME/'"$QUAY_USERNAME"'/' | sed 's/TAG/'"$TAG"'/' > yaml/tmp.yaml
 
-#oc apply -f ../yaml/tmp.yaml -n $EPHEMERAL_NAMESPACE
+oc apply -f yaml/tmp.yaml -n $EPHEMERAL_NAMESPACE
 
 podman run -d -p 2345:2345 quay.io/$QUAY_USERNAME/$QUAY_REPO:$TAG
--- scripts/terminateDebugger.sh
+++ scripts/terminateDebugger.sh
@@ -1,2 +1,4 @@
 rm yaml/tmp.yaml
 podman stop $(podman ps -q --filter ancestor=quay.io/$QUAY_USERNAME/$QUAY_REPO:$TAG )
+
+# oc delete pod ephemeral-debugger-pod -n $EPHEMERAL_NAMESPACE
